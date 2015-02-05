9e979daa548a8ba5fc7a4010c48dcdd674288945
Toni Menzel <toni.menzel@rebaze.com>
2015-02-05 09:47:13 +0100

    adding takari mojo test

--- src/test/java/org/auxis/maven/materialplugin/CreateBillTest.java
+++ src/test/java/org/auxis/maven/materialplugin/CreateBillTest.java
@@ -1,7 +1,29 @@
 package org.auxis.maven.materialplugin;
 
+import io.takari.maven.testing.TestMavenRuntime;
+import io.takari.maven.testing.TestResources;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.File;
+
+import static io.takari.maven.testing.TestMavenRuntime.newParameter;
+import static io.takari.maven.testing.TestResources.assertFilesPresent;
+
 /**
- * Created by tonit on 05/02/15.
+ *
  */
 public class CreateBillTest {
+    @Rule
+    public final TestResources resources = new TestResources();
+
+    @Rule
+    public final TestMavenRuntime maven = new TestMavenRuntime();
+
+    @Test
+    public void test() throws Exception {
+        File basedir = resources.getBasedir("testproject1");
+        maven.executeMojo(basedir, "test", newParameter("bill", "target/output.txt"));
+        assertFilesPresent(basedir, "target/output.txt");
+    }
 }
